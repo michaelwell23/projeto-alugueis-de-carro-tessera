6a9156dd48208591fd4fbdaa1790084b9d73e326
michael wellington lopes <michael23.wellington@gmail.com>
2024-12-20 20:00:31 -0300

    Starting Tessera project: Inserting typing for categories

--- server/src/routes/categories.routes.ts
+++ server/src/routes/categories.routes.ts
@@ -1,18 +1,21 @@
 import { Router } from "express";
 import { v4 as uuidV4 } from "uuid";
+import { Category } from "../model/category";
 
 const categoriesRoutes = Router();
 
-const categories = [];
+const categories: Category[] = [];
 
 categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
 
-  const category = {
-    id: uuidV4(),
+  const category: Category = new Category();
+
+  Object.assign(category, {
     name,
     description,
-  };
+    create_at: new Date(),
+  });
 
   categories.push(category);
 
