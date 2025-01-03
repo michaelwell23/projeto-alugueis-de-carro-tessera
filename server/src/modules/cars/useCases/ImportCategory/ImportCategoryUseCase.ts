ea46ef591105cb3a036e61d8608842d82ef8c884
michael wellington lopes <michael23.wellington@gmail.com>
2025-01-03 13:47:12 -0300

    Working with upload: Getting to know the concept of stream

--- server/src/modules/cars/useCases/ImportCategory/ImportCategoryUseCase.ts
+++ server/src/modules/cars/useCases/ImportCategory/ImportCategoryUseCase.ts
@@ -1,7 +1,18 @@
+import fs from "fs";
+import csvParse from "csv-parse";
+
 export class ImportCategoryUseCase {
   constructor(parameters) {}
 
-  execute(file: Express.Multer.File) {
-    console.log(file);
+  execute(file: Express.Multer.File): void {
+    const stream = fs.createReadStream(file.path);
+
+    const parseFile = csvParse();
+
+    stream.pipe(parseFile);
+
+    parseFile.on("data", async (line) => {
+      console.log(line);
+    });
   }
 }
