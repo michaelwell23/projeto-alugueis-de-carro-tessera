11a69cc1dbf51c552983245f54592880f9e52967
michael wellington lopes <michael23.wellington@gmail.com>
2025-01-28 13:48:58 -0300

    Dependency injection: understanding JTW authentication and password encryption

--- server/src/modules/accounts/useCases/CreateUser/CreateUserUseCase.ts
+++ server/src/modules/accounts/useCases/CreateUser/CreateUserUseCase.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { hash } from "bcrypt";
 import { inject, injectable } from "tsyringe";
 
 import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
@@ -14,14 +16,20 @@ export class CreateUserUseCase {
     name,
     email,
     password,
-    username,
     drive_license,
   }: ICreateUserDTO): Promise<void> {
+    const userAlreadyExists = await this.usersRepository.findByEmail(email);
+
+    if (userAlreadyExists) {
+      throw new Error("User Already exists");
+    }
+
+    const passwordHash = await hash(password, 8);
+
     await this.usersRepository.create({
       name,
       email,
-      password,
-      username,
+      password: passwordHash,
       drive_license,
     });
   }
