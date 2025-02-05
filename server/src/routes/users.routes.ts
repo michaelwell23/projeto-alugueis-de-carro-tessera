a4c5a03224fd0c143044957cb329bfbc83d888f0
michael wellington lopes <michael23.wellington@gmail.com>
2025-02-05 16:01:34 -0300

    User Avatar: Adding avatar column

--- server/src/routes/users.routes.ts
+++ server/src/routes/users.routes.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
 
 import { CreateUserController } from "../modules/accounts/useCases/CreateUser/CreateUserControllers";
+import { UpdateUserAvatarController } from "../modules/accounts/useCases/UpdateUserAvatar/UpdateUserAvatarController";
 
 const usersRouter = Router();
 
 const createUsercontroller = new CreateUserController();
+const updateUserAvatarController = new UpdateUserAvatarController();
 
 usersRouter.post("/", createUsercontroller.handle);
+usersRouter.patch("/avatar", updateUserAvatarController.handle);
 
 export default usersRouter;
