75e91b1684ee351c5b0d8f1b81810f1c41c4433e
michael wellington lopes <michael23.wellington@gmail.com>
2025-02-07 16:27:17 -0300

    User Avatar: removing existing avatar file in temp paste

--- server/src/config/upload.ts
+++ server/src/config/upload.ts
@@ -6,7 +6,7 @@ export default {
   upload(folder: string) {
     return {
       storage: multer.diskStorage({
-        destination: resolve(__dirname, "..", "..", "tmp"),
+        destination: resolve(__dirname, "..", "..", "temp", "avatar"),
         filename: (request, file, callback) => {
           const fileHash = crypto.randomBytes(16).toString("hex");
           const fileName = `${fileHash}-${file.originalname}`;
