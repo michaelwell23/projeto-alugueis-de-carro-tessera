b128e7a84bb435262b05cd7e466768e026e5f2ef
michael wellington lopes <michael23.wellington@gmail.com>
2025-02-15 23:57:42 -0300

    Test and business rules: Refactoring the application (fix)

--- server/src/shared/infra/http/server.ts
+++ server/src/shared/infra/http/server.ts
@@ -21,6 +21,12 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(router);
 
+app.get("/", (req, res) => {
+  res.json({
+    text: "testando!",
+  });
+});
+
 app.use(
   (err: Error, request: Request, response: Response, next: NextFunction) => {
     if (err instanceof AppError) {
