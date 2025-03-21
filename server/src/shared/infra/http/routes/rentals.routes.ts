5445596f3e31cc729cbc81151e72b0250f95b75f
michael wellington lopes <michael23.wellington@gmail.com>
2025-03-21 16:15:05 -0300

    Working with refresh_token and e-mail - Cars: Creating ListRentalsByUser

--- server/src/shared/infra/http/routes/rentals.routes.ts
+++ server/src/shared/infra/http/routes/rentals.routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 
 import { CreateRentalsController } from "@modules/rentals/UseCases/CreateRentals/CreateRentalsController";
 import { DevolutionRentalController } from "@modules/rentals/UseCases/DevolutionRental/DevolutionRentalController";
+import { ListRentalsByUserController } from "@modules/rentals/UseCases/ListRentalsByUser/ListRentalsByUserController";
 
 import { ensureAuthenticated } from "../middleware/ensureAuthenticated";
 
@@ -9,8 +10,10 @@ const rentalRoutes = Router();
 
 const createRentalsController = new CreateRentalsController();
 const devolutionRentalController = new DevolutionRentalController();
+const listRentalsByUserController = new ListRentalsByUserController();
 
 rentalRoutes.post("/", ensureAuthenticated, createRentalsController.handle);
+rentalRoutes.get("/", ensureAuthenticated, listRentalsByUserController.handle);
 rentalRoutes.post(
   "/devolution/:id",
   ensureAuthenticated,
